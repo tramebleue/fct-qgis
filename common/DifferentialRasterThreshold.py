530355be97dce40e89bfc17bad0de0ed5e90cb40
Christophe ROUSSON <c.rousson.gpk@gmail.com>
2018-03-21 19:04:55 +0100

    Remove deprecation warning for gdalnumeric

--- common/DifferentialRasterThreshold.py
+++ common/DifferentialRasterThreshold.py
@@ -29,7 +29,9 @@ from processing.core.parameters import ParameterRaster
 from processing.core.parameters import ParameterNumber
 from processing.core.parameters import ParameterString
 from processing.core.outputs import OutputRaster
-import gdalnumeric as gn
+from processing.core.ProcessingLog import ProcessingLog
+# import gdalnumeric as gn
+from osgeo import gdal
 import numpy as np
 
 
@@ -40,7 +42,7 @@ class DifferentialRasterThreshold(GeoAlgorithm):
     MIN_THRESHOLD = 'MIN_THRESHOLD'
     MAX_THRESHOLD = 'MAX_THRESHOLD'
     RELATIVE_DEM = 'OUTPUT'
-    NO_DATA = 'NO_DATA'
+    # NO_DATA = 'NO_DATA'
 
     def defineCharacteristics(self):
 
@@ -55,8 +57,9 @@ class DifferentialRasterThreshold(GeoAlgorithm):
                                           self.tr('Min. value'), default=-10))
         self.addParameter(ParameterNumber(self.MAX_THRESHOLD,
                                           self.tr('Max. value'), default=10))
-        self.addParameter(ParameterString(self.NO_DATA,
-                                          self.tr('No data value'), '-9999'))
+        
+        # self.addParameter(ParameterString(self.NO_DATA,
+        #                                   self.tr('No data value'), '-9999'))
 
         self.addOutput(OutputRaster(self.RELATIVE_DEM, self.tr('Relative DEM')))
 
@@ -67,15 +70,37 @@ class DifferentialRasterThreshold(GeoAlgorithm):
         minvalue = self.getParameterValue(self.MIN_THRESHOLD)
         maxvalue = self.getParameterValue(self.MAX_THRESHOLD)
         output_path = self.getOutputValue(self.RELATIVE_DEM)
-        ndv = float(self.getParameterValue(self.NO_DATA))
+        # ndv = float(self.getParameterValue(self.NO_DATA))
 
-        dem = gn.LoadFile(input_dem_path)
-        dem = np.ma.masked_where(dem == ndv, dem)
-        reference = gn.LoadFile(reference_dem_path)
-        reference = np.ma.masked_where(reference == ndv, reference)
+        # dem = gn.LoadFile(input_dem_path)
+        ds = gdal.Open(input_dem_path)
+        if ds.RasterCount != 1:
+            ProcessingLog.addToLog(ProcessingLog.LOG_WARNING, self.tr("Input DEM has more than 1 band."))
+
+        dem = ds.GetRasterBand(1).ReadAsArray()
+        nodata = ds.GetRasterBand(1).GetNoDataValue()
+        dem = np.ma.masked_where(dem == nodata, dem)
+        
+        # reference = gn.LoadFile(reference_dem_path)
+        refds = gdal.Open(reference_dem_path)
+        if refds.RasterCount != 1:
+            ProcessingLog.addToLog(ProcessingLog.LOG_WARNING, self.tr("Reference DEM has more than 1 band."))
+        
+        reference = refds.GetRasterBand(1).ReadAsArray()
+        ref_nodata = refds.GetRasterBand(1).GetNoDataValue()
+        reference = np.ma.masked_where(reference == ref_nodata, reference)
+        
         relative = dem - reference
         mask = np.bitwise_and(relative > minvalue, relative <= maxvalue)
 
-        gn.SaveArray(mask.astype(np.uint8), str(output_path), format='GTiff', prototype=str(input_dem_path))
+        # gn.SaveArray(mask.astype(np.uint8), str(output_path), format='GTiff', prototype=str(input_dem_path))
+        driver = gdal.GetDriverByName('GTiff')
+        dst = driver.CreateCopy(str(output_path), ds, strict=0, options=['TILED=YES', 'COMPRESS=DEFLATE'])
+        dst.GetRasterBand(1).WriteArray(mask.astype(np.uint8))
+
+        # Properly close GDAL resources
+        ds = None
+        refds = None
+        dst = None
 
 
