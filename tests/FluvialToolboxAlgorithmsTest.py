52264e3e8a15166ec7f23ca120fc13df082edfeb
Christophe ROUSSON <c.rousson.gpk@gmail.com>
2018-04-23 21:34:46 +0200

    Tests bootstrap

---
+++ tests/FluvialToolboxAlgorithmsTest.py
@@ -0,0 +1,71 @@
+import os
+import sys
+import yaml
+
+PREFIX_PATH = os.environ.get('PREFIX_PATH', '/usr/share')
+sys.path.append(os.path.join(PREFIX_PATH, 'qgis/python/plugins'))
+sys.path.append(os.path.join(os.environ['HOME'], '.qgis2/python/plugins'))
+
+
+from processing.tests import AlgorithmsTestBase
+
+import nose2
+import shutil
+
+from qgis.testing import (
+    start_app,
+    unittest
+)
+
+def processingTestDataPath():
+    # return os.path.join(os.path.dirname(__file__), 'testdata')
+    return 'testdata'
+
+
+class TestFluvialToolboxAlgorithms(unittest.TestCase, AlgorithmsTestBase.AlgorithmsTest):
+
+    @classmethod
+    def setUpClass(cls):
+        start_app()
+        from processing.core.Processing import Processing
+        from fluvialtoolbox.FluvialToolbox import FluvialToolboxProvider
+        Processing.initialize()
+        provider = FluvialToolboxProvider()
+        Processing.addProvider(provider)
+        cls.cleanup_paths = []
+
+    @classmethod
+    def tearDownClass(cls):
+        for path in cls.cleanup_paths:
+            shutil.rmtree(path)
+
+    def test_algorithms(self):
+        """
+        This is the main test function. All others will be executed based on the definitions in testdata/tests.yaml
+        """
+        # ver = processing.version()
+        # print("Processing {}.{}.{}".format(ver / 10000, ver / 100 % 100, ver % 100))
+        with open(os.path.join('testdata', self.test_definition_file()), 'r') as stream:
+            algorithm_tests = yaml.load(stream)
+
+        for algtest in algorithm_tests['tests']:
+            yield self.check_algorithm, algtest['name'], algtest
+
+    def filepath_from_param(self, param):
+        """
+        Creates a filepath from a param
+        """
+        prefix = processingTestDataPath()
+        if 'location' in param and param['location'] == 'qgs':
+            prefix = unitTestDataPath()
+        elif 'location' in param:
+            prefix = param['location']
+
+        return os.path.join(prefix, param['name'])
+
+    def test_definition_file(self):
+        return 'tests.yaml'
+
+
+if __name__ == '__main__':
+    nose2.main()
