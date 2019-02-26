96a0439ddcfdb2deeb6572b3d7377cb6e2fe1c34
samuel7569 <samuel.dunesme@univ-lyon2.fr>
2019-02-26 16:40:42 +0100

    Add workflows

---
+++ fct/workflows/metrics/__init__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+"""
+Metrics Workflows
+
+***************************************************************************
+*                                                                         *
+*   This program is free software; you can redistribute it and/or modify  *
+*   it under the terms of the GNU General Public License as published by  *
+*   the Free Software Foundation; either version 2 of the License, or     *
+*   (at your option) any later version.                                   *
+*                                                                         *
+***************************************************************************
+"""
+
+from .Morphometry import Morphometry
